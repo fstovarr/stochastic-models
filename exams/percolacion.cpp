51e995ce80d4557b13ee240fa82ff857835bb8f7
Fabio Tovar <fabiostovarr@gmail.com>
2020-04-25 18:03:11 -0500

    Improvements done

--- exams/percolacion.cpp
+++ exams/percolacion.cpp
@@ -12,7 +12,12 @@ using namespace std;
 #define DELTA_P 0.001
 #define PAD 8
 
-bool find_path(int m, int n, bool **rock) {
+void clear(queue<pair<int, int>> &q) {
+   queue<pair<int, int>> empty;
+   swap(q, empty);
+}
+
+bool find_path(int m, int n, bool **rock, int possible_moves[][2]) {
     int void_top = n, void_bottom = n;
 
     for (int i = 0; i < n; i++) {
@@ -28,21 +33,19 @@ bool find_path(int m, int n, bool **rock) {
     bool visited[count][m][n];
     memset(visited, false, count * m * n * sizeof(bool));
     
-    queue<pair<int, int>> to_visit[count];
-
-    int possible_moves[][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    queue<pair<int, int>> to_visit;
 
     pair<int, int> current;
     int c_x = 0, c_y = 0, goal = top ? (m - 1) : 0, start = top ? 0 : (m - 1);
 
     for (int i = 0, c = 0; i < m && c < count; i++) {
         if(rock[start][i] == true) continue;
-        to_visit[c].push(make_pair(start, i));
+        to_visit.push(make_pair(start, i));
         visited[c][start][i] = true;
 
-        while(to_visit[c].size() > 0) {
-            current = to_visit[c].front();
-            to_visit[c].pop();
+        while(to_visit.size() > 0) {
+            current = to_visit.front();
+            to_visit.pop();
 
             if(current.first == goal)
                 return true;
@@ -52,13 +55,16 @@ bool find_path(int m, int n, bool **rock) {
                 c_x = current.second + possible_moves[j][1];
                 if (c_x < n && c_x >= 0 && c_y < m && c_y >= 0 && !visited[c][c_y][c_x] && rock[c_y][c_x] == false) {
                     // printf("(%d, %d)\n", c_y, c_x);
-                    to_visit[c].push(make_pair(c_y, c_x));
+                    to_visit.push(make_pair(c_y, c_x));
                     visited[c][c_y][c_x] = true;
                 }
             }
         }
         c++;
+        clear(to_visit);
     }
+
+    // free(visited);
 
     return false;
 }
@@ -106,14 +112,17 @@ int main(int argc, char *argv[]) {
     int iterations = (1 / DELTA_P);
     double results[ iterations * PAD];
 
+    int possible_moves[][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
     printf("p, theta_p\n");
 
-    #pragma omp parallel for
+    #pragma omp parallel for schedule (guided)
     for (int p = 0; p < iterations; p++) {
         double total = 0;
+        bool **rock;
         for (int i = 0; i < ROCKS_PER_SIMULATION; i++) {
-            bool **rock = simulate_rock(M, N, p * 1.0 * DELTA_P, i);
-            total += find_path(M, N, rock);
+            rock = simulate_rock(M, N, p * 1.0 * DELTA_P, i);
+            total += find_path(M, N, rock, possible_moves);
         }
         results[p * PAD] = total;
     }
--- exams/run.sh
+++ exams/run.sh
@@ -3,12 +3,12 @@
 g++ percolacion.cpp -o percolacion -fopenmp -pthread
 
 echo "Running (2, 100)"
-./percolacion 10000 2 100
-echo "Running (100, 2)"
-./percolacion 10000 100 2
-echo "Running (30, 70)"
-./percolacion 10000 30 70
-echo "Running (70, 30)"
-./percolacion 10000 70 30
-echo "Running (200, 200)"
-./percolacion 10000 200 200
+time ./percolacion 1000 2 100
+# echo "Running (100, 2)"
+# ./percolacion 10000 100 2
+# echo "Running (30, 70)"
+# ./percolacion 10000 30 70
+# echo "Running (70, 30)"
+# ./percolacion 10000 70 30
+# echo "Running (200, 200)"
+# ./percolacion 10000 200 200
