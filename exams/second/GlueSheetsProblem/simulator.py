529fa9a71f64b8bddc51a41cbeff0865378dbd2c
Fabio Tovar <fstovarr@unal.edu.co>
2020-06-23 11:49:55 -0500

    Final version

--- exams/second/GlueSheetsProblem/simulator.py
+++ exams/second/GlueSheetsProblem/simulator.py
@@ -3,14 +3,14 @@ from datetime import datetime
 from pathlib import Path
 
 class Simulator():
-    def __init__(self, env, agents, time=400, verbose=False):
+    def __init__(self, env, agents, limit_time=4000, verbose=False):
         self.__time = 0
         self.__env = env
         self.__agents = agents
-        self.__limit_time = time
+        self.__limit_time = limit_time
         self.__full_agents = []
         self.__general_filename = "data/data.csv"
-        self.__specific_filename = "data/{}_.csv".format(datetime.now().strftime("%d%m%Y%H%M%S"))
+        self.__specific_filename = "data/{}_{}_{}.csv".format(self.__env.get_sheets_count(), len(self.__agents), datetime.now().strftime("%d%m%Y%H%M%S"))
         self.__verbose = verbose
         
         self.__init_file()
@@ -20,7 +20,7 @@ class Simulator():
     
     def start(self):
         full_agents = []
-        while (self.__limit_time != None and self.__limit_time > self.__time) or self.__limit_time == None:
+        while self.__limit_time > self.__time:
             agents = self.buy_stage()
             if len(agents) > 0:
                 full_agents.extend(agents)
@@ -31,17 +31,20 @@ class Simulator():
             
             if self.__verbose:
                 print(' '.join(str(a[0]) for a in full_agents))
-                print(str(self.__agents[7]))
                 print("Total: {}\tCompleted: {}".format(len(self.__agents), len(self.__full_agents)))
                 print("-------------------------\n")
             
             self.__full_agents.extend(full_agents)
             self.__collect_metrics(full_agents)
 
-            if self.__limit_time == None and len(self.__full_agents) == len(self.__agents):
+            if len(self.__full_agents) == len(self.__agents):
                 break
             self.__time += 1
             full_agents = []
+        
+        if self.__limit_time == self.__time:
+            print("LIMIT REACHED in case {} {}".format(self.__env.get_sheets_count(), len(self.__agents)))
+
         print("Simulation ended")
 
     def buy_stage(self):
@@ -64,22 +67,25 @@ class Simulator():
         for agent in self.__agents:
             if agent.has_surplus():
                 flags[0] = agent.get_state() == AgentState.LOOKING_FOR_SHEETS
-                (friend_list, own_list, friend) = agent.look_for_exchange()
-                
-                flags[1] = friend.get_state() == AgentState.LOOKING_FOR_SHEETS
-                agent.do_exchange(friend_list, own_list, friend)
+                possible_exchange = agent.look_for_exchange()
 
-                if flags[0] and agent.get_state() == AgentState.COMPLETED:
-                    full_agents.append([agent, "exchange"])
-                if flags[1] and friend.get_state() == AgentState.COMPLETED:
-                    full_agents.append([friend, "exchange"])
+                if possible_exchange != None:
+                    (friend_list, own_list, friend) = possible_exchange
+                    flags[1] = friend.get_state() == AgentState.LOOKING_FOR_SHEETS
+                    agent.do_exchange(friend_list, own_list, friend)
+
+                    if flags[0] and agent.get_state() == AgentState.COMPLETED:
+                        full_agents.append([agent, "exchange"])
+                    
+                    if flags[1] and friend.get_state() == AgentState.COMPLETED:
+                        full_agents.append([friend, "exchange"])
         return full_agents
 
     ### Private functions
     def __init_file(self):
         # TODO time,sheets,id,friends,surplus,agents,full_agents,stage
         Path(self.__general_filename).touch()
-        Path(self.__specific_filename).touch()
+        # Path(self.__specific_filename).touch()
 
     def __collect_metrics(self, agents):
         metrics = ""
@@ -87,8 +93,8 @@ class Simulator():
             metrics = "{},{},{},{},{},{}\n".format(self.__time, self.__env.get_sheets_count(), agent.get_metrics(), len(self.__agents), len(self.__full_agents), stage)
         if metrics != "":
             f = open(self.__general_filename, "a+")
-            f2 = open(self.__specific_filename, "a+")
+            # f2 = open(self.__specific_filename, "a+")
             f.write(metrics)
-            f2.write(metrics)
+            # f2.write(metrics)
             f.close()
-            f2.close()
+            # f2.close()
